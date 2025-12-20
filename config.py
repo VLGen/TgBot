fb9375f29aec05a699867ac57394196d8dcb75c6
Sergey Zhigalko <szhigalko79@gmail.com>
2025-12-20 14:56:37 +0500

    Обновление проекта: конфигурация, база, логика

--- config.py
+++ config.py
@@ -1,2 +1,17 @@
-API_TOKEN = "7614797287:AAF46Nl36fRj2cX3AtgBUGFvHNPHIrmXS_Y"
-DB_NAME = "quiz_bot.db"
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+from typing import Optional
+
+BASE_DIR = Path(__file__).resolve().parent
+
+class Settings(BaseSettings):
+    bot_token: Optional[SecretStr] = None
+    db: Optional[str] = None
+
+    model_config = SettingsConfigDict(
+        env_file=str(BASE_DIR / ".env"),
+        env_file_encoding='utf-8'
+    )
+
+config = Settings()
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import asyncio
 import logging
 from aiogram import Dispatcher, Bot
-from config import API_TOKEN
+from config import config
 from database import create_table
 from handlers import register_handlers
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=API_TOKEN)
+print("BOT_TOKEN RAW:", config.bot_token)
+print("BOT_TOKEN VALUE:", None if config.bot_token is None else config.bot_token.get_secret_value())
+print("DB:", config.db)
+
+bot = Bot(token=config.bot_token.get_secret_value()) # type: ignore
 dp = Dispatcher()
 
 register_handlers(dp)
